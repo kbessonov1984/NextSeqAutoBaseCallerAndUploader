5c30e9994c0dd49610314c41fe80b936e6f72cc0
Kirill Bessonov <kbessonov@gmail.com>
2019-07-04 13:38:17 -0400

    Added automatic IRIDA server upload and created option to re-upload previously basecalled folder

--- NextSeqPostRunDataProcessor.sh
+++ NextSeqPostRunDataProcessor.sh
@@ -8,20 +8,105 @@
 #notes           :Tested on Linux system with illumina conda environment containing bcl2fastq basecaller
 #bash_version    :4.2.46(2)-release
 #==============================================================================
+upload_basecalled_data2IRIDA () {
+   	# Uploads fastqs to the IRIDA server projects specified in the SampleSheet.csv file
+   	# Inputs: ${1} is the run_folder name that will be used to upload samples
+   	echo "Make sure that you have IRIDA uploader config file (irida-uploader-config.conf) in the running directory"
+   	echo "You are now located in $(pwd) directory"
+   	echo "Upload details are written to Log_IridaUpload_$(date +"%m-%d-%Y").log file. Check for any errors."
+   	logfilename="Log_IridaUpload_$(date +"%m-%d-%Y").log" && touch  ${logfilename}
+   	runid=$(sbatch -c 1 --mem=2G -p "NMLResearch" -J "NextSeqDataUpload" --time=24:00:00 -o "${logfilename}" --wrap="source activate irida-uploader && irida-uploader --config ./irida-uploader-config.conf -f ${1}"| grep -oE  "[0-9]+")
+   	echo "Started upload job ${runid}"
+   	bcstatus=1 #basecalling not yet completed
+	while [ $bcstatus != 0 ];do
+	  bcstatus=$(squeue | awk '{print $1}'| grep -w ${runid} | wc -l)
+	  echo "$(date +"%T"): IRIDA Uploader is uploading samples according to SampleSheet.csv. Will check back on job ${runid} in 1 min ..."
+	  tail ${logfilename}
+	  sleep 60
+	done
+} 
 
 echo "###################################################"
-echo "# Welcome to the NextSeq Post-Run Processor v1.0  #"
+echo "# Welcome to the NextSeq Post-Run Processor v2.0  #"
 echo "###################################################"
-
-echo "Current directoy: $(pwd)"
-echo "Available projects: $(ls -d */ | sed -e 's/\///g')"
 echo ""
 
-read -p "Enter Run Name Folder (e.g. 190329_NB551633_0004_AHGCNCAFXY): "  runfolder
+read -p "Re-upload previously basecalled samples for a given project? (Y/N)" user_response
+if [ $user_response == "Y" ];then
+	echo "Current directoy: $(pwd)"
+	echo "Available projects: $(ls -d */ | sed -e 's/\///g')"
+	read -p "Enter Run Name Folder (e.g. 190329_NB551633_0004_AHGCNCAFXY): "  runfolder
+	upload_basecalled_data2IRIDA $runfolder
+	exit 0
+else
+	echo ""
+fi
+
+latest_runfolder=($(ls -St | grep -E [0-9]+.*_.*[A-Z]+))
+latest_runfolder=${latest_runfolder[0]}
+echo "Current directoy: $(pwd)"
+echo "The latest run folder name is \"${latest_runfolder}\""
+read -p "Run basecalling and automatic IRIDA upload on \"${latest_runfolder}\" folder? (Y/N) " user_response
+if [ $user_response == "Y" ]; then
+	runfolder=${latest_runfolder}
+else
+	echo "Manually enter folder name to run basecalling and automatic upload"
+	echo "Available projects: $(ls -d */ | sed -e 's/\///g')"
+	read -p "Enter Run Name Folder (e.g. 190329_NB551633_0004_AHGCNCAFXY): "  runfolder
+fi
+
+
+
 if [ -d "$runfolder" ]; then
-  echo "Folder ${runfolder} exists. Activating illumina environment."
-  source activate illumina
-  cd ${runfolder}
+  	echo "Folder ${runfolder} exists"
+	echo "Starting basecalling ... Should be done in 15 min if server load is low ..."
+	basecall_log_file="Log_NextSeqPostRunDataProcessor_$(date +"%m-%d-%Y").log"
+	runid=$(sbatch -c 16 --mem=32G  --time=24:00:00 -p "NMLResearch" -J "NextSeqBasecalling" -o "${basecall_log_file}" --wrap="source activate illumina && cd ${runfolder} && bcl2fastq --no-lane-splitting --sample-sheet SampleSheet.csv" | grep -oE  "[0-9]+")
+
+
+	bcstatus=1 #basecalling not yet completed
+	while [ $bcstatus != 0 ];do
+	  bcstatus=$(sq | awk '{print $1}'| grep -w ${runid} | wc -l)
+	  echo "$(date +"%T"): NextSeq basecalling is still runing as jobid ${runid}. Will check back in 1 min ..."
+	  sleep 60
+	done
+
+	echo "$(date +"%T"): Gathering location of the basecalled FASTQ files ..."
+	fastqs=($(grep "Created FASTQ file" ${basecall_log_file} | awk '{print $8}' | sed -e "s/[\"|\']//g"))
+	destination_dir=$(echo ${fastqs[0]} | grep -oE ".*BaseCalls")
+
+	if [ ${#fastqs[@]} -eq 0 ] || [ ${#destination_dir} -eq 0 ];then
+		echo "$(date +"%T"): Basecaller failed or generated 0 FASTQ files. Aborting ..."
+		exit 1
+	else
+		echo "$(date +"%T"): Basecaller generated fastq files"
+		for fastq in ${fastqs[@]};do
+			mv ${fastq} ${destination_dir} 2>/dev/null
+		done
+		echo "$(date +"%T"): FASTQ files were moved to ${destination_dir}"
+
+		#rename files to a new pattern with "_L001_" added to the file name (pools all samples from the 4 lanes)
+		cd $destination_dir
+		echo "Renaming files to *_L001_R1_* or  *_L001_R2_* pattern as per irida-uploader requirements (*.L\d{3}_R(\d+)_\S+\.fastq.*)"
+		for file in *.gz;do
+			file_new=$(echo $file | sed -e 's/\(_R[12]_\)/_L001\1/g')
+			mv $file $file_new 
+		done	
+
+		printf '=%.0s' {1..100} &&  echo ""
+		echo "$(date +"%T"): Basecall run with jobid ${runid} completed generating a total of $(ls ${destination_dir}/*.fastq.gz  | wc -l) fastq.gz files"
+		
+		echo "Running automatic sample upload to IRIDA server in 10s. "
+		sleep 10
+		upload_basecalled_data2IRIDA ${runfolder} #upload data to IRIDA automatically
+		
+	fi
+	echo "------------Run completed-----------------"
+	
+
+	
+
+
 else
   echo "The ${runfolder} run folder does not exist at current location $(pwd)"
   echo "Following directories are available at the current location: $(ls -d */)"
@@ -29,43 +114,3 @@ else
   exit 1
 fi
 
-echo "Starting basecalling ...."
-runid=$(sbatch -c 16 --mem=32G -p "NMLResearch" -J "NextSeqBasecalling" -o "NextSeqBasecallingLog_${runfolder}.txt" --wrap="bcl2fastq --no-lane-splitting --sample-sheet *.csv" | grep -oE  "[0-9]+")
-#runid=$(sbatch -c 1 --mem=1G -J "NextSeqBasecalling" -o "NextSeqBasecallingLog_${runfolder}.txt" --wrap="find . -name "*.gz" | grep -oE  "[0-9]+")
-
-
-bcstatus=1 #basecalling not yet completed
-while [ $bcstatus != 0 ];do
-  bcstatus=$(sq | awk '{print $1}'| grep -w ${runid} | wc -l)
-  echo "$(date +"%T"): NextSeq basecalling is still runing as jobid ${runid}. Will check back in 1 min ..."
-  sleep 60
-done
-
-echo "$(date +"%T"): Gathering location of the basecalled FASTQ files ..."
-fastqs=($(grep "Created FASTQ file" NextSeqBasecallingLog_${runfolder}.txt | awk '{print $8}' | sed -e "s/[\"|\']//g"))
-destination_dir=$(echo ${fastqs[0]} | grep -oE ".*BaseCalls/")
-
-if [ ${#fastqs[@]} -eq 0 ] || [ ${#destination_dir} -eq 0 ];then
-	echo "$(date +"%T"): Basecaller failed or generated 0 FASTQ files. Aborting ..."
-	exit 1
-else
-	echo "$(date +"%T"): Basecaller generated ${#fastqs[@]} fastq files"
-fi
-
-
-for fastq in ${fastqs[@]};do
-	mv ${fastq} ${destination_dir} 2>/dev/null
-done
-
-echo "$(date +"%T"): FASTQ files were moved to ${destination_dir}"
-
-#rename files to a new pattern with "_L001_" added to the file name (pools all samples from the 4 lanes)
-cd $destination_dir
-for file in *.gz;do
-	file_new=$(echo $file | sed -e 's/\(_R[12]_\)/_L001\1/g')
-	mv $file $file_new 
-done	
-
-printf '=%.0s' {1..100} &&  echo ""
-echo "$(date +"%T"): Basecall run# ${runid} completed generating a total of $(ls ${destination_dir}/*.fastq.gz  | wc -l)"
-echo "Run IRIDA uploader on the ${destination_dir} directory and upload files to IRIDA (http://ngs-archive.corefacility.ca/irida/)"
--- README.md
+++ README.md
@@ -3,3 +3,55 @@
 This bash script automates NextSeq Illumina raw data processing by generating ready to upload FASTQ files. The FASTQ files are basecalled using [bcl2fastq Illumina software](https://support.illumina.com/sequencing/sequencing_software/bcl2fastq-conversion-software.html). The final raw sequences can be uploaded to NML IRIDA server via the [IRIDA Uploader](https://github.com/phac-nml/irida-miseq-uploader).
 
 For more information please consult the [documentation website](https://kbessonov1984.github.io/NextSeqAutoBaseCallerAndUploader/)
+
+Local Config
+------------
+Local configuration file to try out for the command line upload. It works if config file is structured correctly. See example config file [here](https://raw.githubusercontent.com/phac-nml/irida-uploader/a77521d440aa4c2ea5412714f855625edeaad91b/tests/config/example_config.conf).
+
+Example run on waffles server and irida command line uploader
+
+```
+/Drives/P/conda_envs/irida-uploader-0.2.1) [kbessono@waffles Data]$ irida-uploader --config config-v2.conf -f 190329_NB551633_0004_AHGCNCAFXY/
+2019-07-03 21:43:42 INFO     looking for run in 190329_NB551633_0004_AHGCNCAFXY/
+2019-07-03 21:43:42 INFO     Adding log file to 190329_NB551633_0004_AHGCNCAFXY/
+2019-07-03 21:43:42 INFO     ==================================================
+2019-07-03 21:43:42 INFO     ---------------STARTING UPLOAD RUN----------------
+2019-07-03 21:43:42 INFO     Uploader Version 0.2.1
+2019-07-03 21:43:42 INFO     Logging to file in: /home/CSCScience.ca/kbessono/.cache/irida_uploader/log
+2019-07-03 21:43:42 INFO     ==================================================
+2019-07-03 21:43:42 INFO     Reading data from sample sheet 190329_NB551633_0004_AHGCNCAFXY/SampleSheet.csv
+2019-07-03 21:43:43 INFO     Looking for files with pattern SA20190644n_S1_L\d{3}_R(\d+)_\S+\.fastq.*$
+2019-07-03 21:43:43 INFO     Looking for files with pattern SA20191119n_S2_L\d{3}_R(\d+)_\S+\.fastq.*$
+2019-07-03 21:43:43 INFO     Looking for files with pattern SA20191120n_S3_L\d{3}_R(\d+)_\S+\.fastq.*$
+2019-07-03 21:43:49 INFO     Getting samples from project '938'
+2019-07-03 21:44:00 INFO     Creating sample 'SA20190644n' for project '938' on IRIDA.
+2019-07-03 21:44:11 INFO     Getting samples from project '938'
+2019-07-03 21:44:24 INFO     Getting samples from project '938'
+2019-07-03 21:44:24 INFO     Creating sample 'SA20191119n' for project '938' on IRIDA.
+2019-07-03 21:44:36 INFO     Getting samples from project '938'
+2019-07-03 21:44:49 INFO     Getting samples from project '938'
+2019-07-03 21:44:49 INFO     Creating sample 'SA20191120n' for project '938' on IRIDA.
+
+2019-07-03 21:45:13 INFO     *** Starting Upload ***
+2019-07-03 21:45:18 INFO     Sequencing run id '2943' has been created for upload
+2019-07-03 21:45:23 INFO     Uploading to Sample SA20190644n on Project 938
+2019-07-03 21:45:34 INFO     Starting to send file 190329_NB551633_0004_AHGCNCAFXY/Data/Intensities/BaseCalls/SA20190644n_S1_L001_R2_001.fastq.gz
+Progress:  100.0 % Uploaded
+2019-07-03 21:45:36 INFO     Finished sending file 190329_NB551633_0004_AHGCNCAFXY/Data/Intensities/BaseCalls/SA20190644n_S1_L001_R2_001.fastq.gz
+2019-07-03 21:45:36 INFO     Starting to send file 190329_NB551633_0004_AHGCNCAFXY/Data/Intensities/BaseCalls/SA20190644n_S1_L001_R1_001.fastq.gz
+Progress:  100.0 % Uploaded
+2019-07-03 21:45:38 INFO     Finished sending file 190329_NB551633_0004_AHGCNCAFXY/Data/Intensities/BaseCalls/SA20190644n_S1_L001_R1_001.fastq.gz
+2019-07-03 21:45:40 INFO     Uploading to Sample SA20191119n on Project 938
+2019-07-03 21:45:53 INFO     Starting to send file 190329_NB551633_0004_AHGCNCAFXY/Data/Intensities/BaseCalls/SA20191119n_S2_L001_R2_001.fastq.gz
+Progress:  100.0 % Uploaded
+2019-07-03 21:45:56 INFO     Finished sending file 190329_NB551633_0004_AHGCNCAFXY/Data/Intensities/BaseCalls/SA20191119n_S2_L001_R2_001.fastq.gz
+2019-07-03 21:45:56 INFO     Starting to send file 190329_NB551633_0004_AHGCNCAFXY/Data/Intensities/BaseCalls/SA20191119n_S2_L001_R1_001.fastq.gz
+Progress:  100.0 % Uploaded
+2019-07-03 21:46:23 INFO     Samples in directory '190329_NB551633_0004_AHGCNCAFXY/' have finished uploading!
+2019-07-03 21:46:23 INFO     ==================================================
+2019-07-03 21:46:23 INFO     ----------------ENDING UPLOAD RUN-----------------
+2019-07-03 21:46:23 INFO     ==================================================
+2019-07-03 21:46:23 INFO     Stopped active logging to run directory
+```
+
+
